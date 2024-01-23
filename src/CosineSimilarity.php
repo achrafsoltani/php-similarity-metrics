1d2079ccdd10ec9bcca4f54f03e76e5512850a17
thenixboy <thenixboy@pm.me>
2024-01-23 15:36:23 +0100

    Cosine similarity reimplementation

--- src/CosineSimilarity.php
+++ src/CosineSimilarity.php
@@ -4,18 +4,71 @@ namespace AchrafSoltani\PhpSimilarityMetrics;
 
 class CosineSimilarity implements ComputeInterface
 {
-    private $vector1;
-    private $vector2;
-    public function __construct(Array $v1, Array $v2){
-        $this->vector1 = $v1;
-        $this->vector2 = $v2;
+    private $matrix;
+    private $vector;
+    public function __construct(Array $m, Array $v){
+        $this->matrix = $m;
+        $this->vector = $v;
     }
     public function compute()
     {
-        return $this->dotProduct($this->vector1,$this->vector2)/sqrt($this->dotProduct($this->vector1,$this->vector2)*$this->dotProduct($this->vector1,$this->vector2));
+        /*
+         * Input example
+         * $this->matrix = array(
+                            array(
+                                "element" => "identifier",
+                                "tags" => array("key1", "key2", "...", "keyN")
+                            ),
+                            array(
+                                "element" => "identifier",
+                                "tags" => array("key1", "key2", "...", "keyN")
+                            ),
+                            array(
+                                "element" => "identifier",
+                                "tags" => array("key1", "key2", "...", "keyN")
+                            ),
+                            array(
+                                "element" => "identifier",
+                                "tags" => array("key1", "key2", "...", "keyN")
+                            )
+                        );
+         */
+        $dot = Similarity::dot(call_user_func_array("array_merge", array_column($this->matrix, "tags")));
+        // $this->vector = array('key1', 'key2', 'keyN');
+        foreach($this->matrix as $element) {
+            $score[$element['element']] = Similarity::cosine($this->vector, $element['tags'], $dot);
+        }
+        asort($score);
+        return $score;
     }
 
-    function dotProduct($arr1, $arr2){
-        return array_sum(array_map(function($a, $b) { return $a * $b; }, $arr1, $arr2));
+    public function dot($tags) {
+        $tags = array_unique($tags);
+        $tags = array_fill_keys($tags, 0);
+        ksort($tags);
+        return $tags;
+    }
+    public function dot_product($a, $b) {
+        $products = array_map(function($a, $b) {
+            return $a * $b;
+        }, $a, $b);
+        return array_reduce($products, function($a, $b) {
+            return $a + $b;
+        });
+    }
+    public function magnitude($point) {
+        $squares = array_map(function($x) {
+            return pow($x, 2);
+        }, $point);
+        return sqrt(array_reduce($squares, function($a, $b) {
+            return $a + $b;
+        }));
+    }
+    public function cosine($a, $b, $base) {
+        $a = array_fill_keys($a, 1) + $base;
+        $b = array_fill_keys($b, 1) + $base;
+        ksort($a);
+        ksort($b);
+        return $this->dot_product($a, $b) / ($this->magnitude($a) * $this->magnitude($b));
     }
 }
